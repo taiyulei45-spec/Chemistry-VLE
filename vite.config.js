26dfc3f2bb2558a7cc78af33fafcb99b8d881360
TAI 邰 <taitai@TAIdeMacBook-Air.local>
2026-04-07 00:34:19 +0800

    fix: update vite config for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Chemistry-VLE/', // 别忘了保留你之前配的 GitHub Pages 路径
+  build: {
+    chunkSizeWarningLimit: 1500, // 将警告限制调高到 1500 kB
+  }
 })
